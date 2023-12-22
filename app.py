ce7ce9008f6a08d3c0b40bfc3c9f0c53d4b7b836
kishorravi <ece.kishor@gmail.com>
2023-12-22 20:19:26 +0000

    Update app.py
    Updated

--- app.py
+++ app.py
@@ -2,27 +2,38 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-image = cv2.imread("/content/car.jpeg")
-height, width, _ = image.shape  # Added to get height and width of the image
+# Load the image
+image = cv2.imread("path/to/your/image.jpg")  # Replace with the path to your image
 
+# Get image dimensions
+height, width, _ = image.shape
+
+# Create a new image with RGB color order for displaying with Matplotlib
 new_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 plt.imshow(new_image)
 
-yolo = cv2.dnn.readNet("/content/yolov5s.onnx")  # Use the path to the YOLOv5 ONNX file
+# Load YOLOv5 ONNX model
+yolo = cv2.dnn.readNet("yolov5s.onnx")  # Replace with the path to your yolov5s.onnx file
 
+# Load classes
 classes = []
-with open("/content/coco.names", 'r') as f:
+with open("coco.names", 'r') as f:
     classes = f.read().splitlines()
 
+# Preprocess the image
 blob = cv2.dnn.blobFromImage(image, 1 / 255, (640, 640), (0, 0, 0), swapRB=True, crop=False)
 yolo.setInput(blob)
+
+# Get YOLOv5 output
 output_layer_name = yolo.getUnconnectedOutLayersNames()
 layer_output = yolo.forward(output_layer_name)
 
+# Initialize lists to store bounding boxes, confidences, and class IDs
 boxes = []
 confidences = []
 class_ids = []
 
+# Process each detection
 for output in layer_output:
     for detection in output:
         scores = detection[5:]
@@ -42,9 +53,11 @@ for output in layer_output:
             confidences.append(float(confidence))
             class_ids.append(class_id)
 
+# Apply non-maximum suppression to remove overlapping boxes
 indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
 indexes_array = np.array(indexes).flatten()
 
+# Draw bounding boxes on the image
 font = cv2.FONT_HERSHEY_PLAIN
 colors = np.random.uniform(0, 255, size=(len(boxes), 3))
 
@@ -56,5 +69,6 @@ for i in indexes_array:
     cv2.rectangle(new_image, (x, y), (x + w, y + h), color, 7)
     cv2.putText(new_image, label + " " + confi, (x, y + 20), font, 2, (255, 255, 255), 5)
 
+# Display the image with bounding boxes
 plt.imshow(new_image)
 plt.show()
